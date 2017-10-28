d01e00b90badcf9d5ed1e2dde8e481fc2429557b
Adulvitc <adulvitc@gmail.com>
2017-10-28 15:35:04 +0700

    Back On toolbar success

--- app/src/main/java/com/betagro/adulvitc/adulvitcservice/fragment/RegisterFragment.java
+++ app/src/main/java/com/betagro/adulvitc/adulvitcservice/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package com.betagro.adulvitc.adulvitcservice.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.betagro.adulvitc.adulvitcservice.MainActivity;
 import com.betagro.adulvitc.adulvitcservice.R;
 
 /**
@@ -14,6 +16,35 @@ import com.betagro.adulvitc.adulvitcservice.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }// Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).setTitle(getResources().getString(R.string.new_register));
+
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+
+    }
 
     @Nullable
     @Override
